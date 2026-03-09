b30695674907b3da8fba4e0f6c076eb680d40486
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2026-03-09 09:58:34 +0700

    add เปลี่ยน ip ใหม่

--- dbconnect.ts
+++ dbconnect.ts
@@ -1,17 +1,10 @@
 import { createPool } from 'mysql2/promise';
 
-// export const conn = createPool({
-//     connectionLimit: 10,
-//     host: '191.101.230.103',
-//     user: 'u528477660_mypdf',
-//     password: '6TsoYqL5/7g^1>>#',
-//     database: 'u528477660_mypdf',
-// });
-
 export const conn = createPool({
     connectionLimit: 10,
-    host: '10.33.4.47',
-    user: 'saifa',
-    password: 'password12356',
-    database: 'db_saifa',
+    host: '194.59.164.133',
+    user: 'u528477660_mypdf',
+    password: '6TsoYqL5/7g^1>>#',
+    database: 'u528477660_mypdf',
 });
+
